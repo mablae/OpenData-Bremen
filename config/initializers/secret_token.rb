d4dd9d86d82c6f21e7231bd62024de3ab18be6b4
dbloete <mail@dennisreimann.de>
2011-11-13 12:11:09 +0100

    Initial commit with basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bremen::Application.config.secret_token = '791c208a6ad269ad39390f1f7670fd9a716a360c611f88f3a9bb398228384ecf73ac78eea944ca60e248cbe246cd4070919406c13be993d79d599c994f203c59'
