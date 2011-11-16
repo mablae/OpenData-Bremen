60d75063a4adeb1622f1161d560cc5aa83a6a0f1
dbloete <mail@dennisreimann.de>
2011-11-16 11:31:28 +0100

    Geokodierung für Kitas

--- app/models/kita.rb
+++ app/models/kita.rb
@@ -1,4 +1,38 @@
 class Kita < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :adresse
+
+  geocoded_by :addresse do |obj, results|
+    if geo = results.first
+      obj.latitude = geo.latitude
+      obj.longitude = geo.longitude
+      obj.adresse_normalisiert = geo.address
+    end
+  end
+
+  after_validation :geocodieren
+
+  scope :gefiltert, lambda { |params|
+    s = scoped
+    if params[:bounds].present? && params[:bounds].is_a?(Hash)
+      box = params[:bounds].slice(:sw_lat, :sw_lng, :ne_lat, :ne_lng).values
+      s = s.within_bounding_box(box)
+    end
+    s
+  }
+
+  def geocodiert?
+    latitude? && longitude?
+  end
+
+  def geocodieren!
+    geocode
+  end
+
+  private
+
+  def geocodieren
+    geocode if !Rails.env.test? && (new_record? || adresse_changed?)
+    true
+  end
 end
