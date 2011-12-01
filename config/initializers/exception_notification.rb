0a054a097559b23b3d0046a81622b647cbfcaa38
dbloete <mail@dennisreimann.de>
2011-12-01 15:05:45 +0100

    Noch ein Fix

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Holywood::Application.config.middleware.use ExceptionNotifier,
+  Bremen::Application.config.middleware.use ExceptionNotifier,
     email_prefix: "[#{Settings.app_name} #{Rails.env}] ",
     sender_address: %{"Exception Notifier" <info@incodewetrust.com>},
     exception_recipients: %w{mail@dennisreimann.de}
