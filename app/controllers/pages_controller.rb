fa489d573fce6a4c71b154bdef45b581c789692b
Dennis Reimann <mail@dennisreimann.de>
2012-01-13 15:21:54 +0100

    Googlebot Fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+  before_filter :force_request_format_to_html
+
   def index
+  end
+
+  private
+
+  def force_request_format_to_html
+    request.format = :html
   end
 end
